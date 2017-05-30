6c97560a5c9299f711d1dcaba030d49ef64de016
chris <chris@chriss-MacBook-Air.local>
2017-05-29 22:48:40 -0700

    renaming

--- src/pages/Layout/layout.js
+++ src/pages/Layout/layout.js
@@ -19,6 +19,7 @@ export default class Layout extends React.Component {
     }
 
     handleAllClickEvents(event) {
+        debugger;
         //this.state.navTo=event.target.parentNode.getAttribute("data-content");
         var navTo = event.target.parentNode.getAttribute("data-content");
         if (navTo) {
