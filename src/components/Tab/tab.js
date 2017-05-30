3b0e92df8b57251c9346f8e70eaf481c814a0ca1
chris <chris@chriss-MacBook-Air.local>
2017-05-29 23:02:24 -0700

    confused click

--- src/components/Tab/tab.js
+++ src/components/Tab/tab.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 
 export default class Tab extends React.Component {
 
+handleClickEvents (event){
+	debugger;
+}
 
   render() {
     return (
       <div className="tabContainer">
-        <div className="tab" data-content={this.props.dataContent}> {this.props.title} </div>
+        <div className="tab" data-content={this.props.dataContent} onClick={this.handleClickEvents.bind(this)}> {this.props.title} </div>
      </div>
     );
   }
