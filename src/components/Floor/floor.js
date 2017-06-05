860de0d5c2162395c2a5f97eec539ab788370e41
chris <chris@chriss-MacBook-Air.local>
2017-06-05 02:03:36 -0700

    fixing floor

--- src/components/Floor/floor.js
+++ src/components/Floor/floor.js
@@ -5,8 +5,7 @@ export default class Floor extends React.Component {
     
     render() {
      return (
-      <div>
-        <img src="/public/images/floor.jpg" className="floor" />
+      <div className="floor">
          </div>
     );
 }
