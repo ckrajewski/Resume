a245549d187b52581d37f64cbc60971a2fc80dd1
chris <chris@chriss-MacBook-Air.local>
2017-06-06 00:27:58 -0700

    updating descriptions

--- src/components/PersonalDescription/personalDescription.js
+++ src/components/PersonalDescription/personalDescription.js
@@ -7,8 +7,11 @@ export default class PersonalDescription extends React.Component {
       <div className="personalDescription aboutMePersonalDescription">
         <DescriptionContainer title="THE PROFILE PIC" description="About the Artist:">
         <div>
-      	I enjoy learning as much as I can about JavaScript, jQuery, AJAX,RESTful web services, and JavaScript frameworks like React. In fact, I have used all of these technologies when designing 
-      	this site. Through my work at Salesforce  I have also become enveloped with the proper ways of creating an app.
+      	I enjoy learning as much as I can about front end frameworks like Aura and React. In fact, I have used React when designing 
+      	this site. <br/>
+        Through my work at Salesforce I have also become enveloped with the proper ways of creating an app. As a developer, delivering a good product is more than just writing good code.
+        It's questioning whether the design pattern you've chosen is the best one.   It's understanding why you're even writing it. Salesforce encourages these behaviors, and I've taken a more active approach by engaging with leads when designing new features. I've 
+        also learned new languages and tools whenever the opportunity was afforded to me. It's what excites and drives me to do my best at work.
         </div>
         </DescriptionContainer>
       </div>
