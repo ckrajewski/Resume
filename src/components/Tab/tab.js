246e77bc5e57f7b9589e282b889a42120bfd1db2
chris <chris@chriss-MacBook-Air.local>
2017-06-05 21:48:16 -0700

    adding underline

--- src/components/Tab/tab.js
+++ src/components/Tab/tab.js
@@ -7,13 +7,17 @@ handleTabClickEvents (event){
 	var tabs=document.getElementsByClassName("tab");
 	for(var i=0; i<tabs.length; i++){
 		tabs[i].style.color="black";
+		tabs[i].parentElement.style.border="none";
 	}
+	debugger;
 	event.currentTarget.style.color="#AF6622";
+	event.currentTarget.parentElement.style.borderBottom="solid 2px";
+	event.currentTarget.parentElement.style.borderColor="#AF6622";
 }
 
   render() {
     return (
-      <div className="tabContainer"  data-content={this.props.dataContent}>
+      <div className={"tabContainer " + this.props.selectedTabContainerStyle}  data-content={this.props.dataContent}>
         <div className={"tab " + this.props.selectedTabStyle}  data-content={this.props.dataContent} onClick={this.handleTabClickEvents.bind(this)}> {this.props.title} </div>
      </div>
     );
